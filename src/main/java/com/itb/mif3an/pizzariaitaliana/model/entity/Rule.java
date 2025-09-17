ebf0cfc0d3de15c0e4dd4a038a14287f7bab01c9
BarbozaJailson <jailson.100vicio@hotmail.com>
2025-09-16 22:51:43 -0300

    criacao de permission

--- src/main/java/com/itb/mif3an/pizzariaitaliana/model/entity/Rule.java
+++ src/main/java/com/itb/mif3an/pizzariaitaliana/model/entity/Rule.java
@@ -1,7 +1,38 @@
 package com.itb.mif3an.pizzariaitaliana.model.entity;
 
+import java.util.Set;
+
+import static com.itb.mif3an.pizzariaitaliana.model.entity.Permission.*;
+
 public enum Rule {
-    ADMIN,
-    CLIENTE,
-    FUNCIONARIO
+    ADMIN(
+            Set.of(
+                    ADMIN_READ,
+                    ADMIN_UPDATE,
+                    ADMIN_CREATE,
+                    ADMIN_DELETE,
+                    FUNCIONARIO_READ,
+                    FUNCIONARIO_UPDATE,
+                    FUNCIONARIO_CREATE,
+                    FUNCIONARIO_DELETE
+            )
+    ),
+    CLIENTE(
+            Set.of(
+                    CLIENTE_READ,
+                    CLIENTE_UPDATE,
+                    CLIENTE_CREATE,
+                    CLIENTE_DELETE
+            )
+    ),
+    FUNCIONARIO(
+            Set.of(
+                    FUNCIONARIO_READ,
+                    FUNCIONARIO_UPDATE,
+                    FUNCIONARIO_CREATE,
+                    FUNCIONARIO_DELETE
+            )
+    );
+    private final Set<Permission> permissions;
+    Rule(Set<Permission> permissions) {this.permissions = permissions;};
 }
